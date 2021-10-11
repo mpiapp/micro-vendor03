b01780cd1905aa4c00cfe4f9794c85420856a005
Zul <zulkhaery@gmail.com>
2021-10-11 11:35:09 +0700

    fix unit test coverage

--- src/verification/specs/verification.controller.spec.ts
+++ src/verification/specs/verification.controller.spec.ts
@@ -4,10 +4,36 @@ import { VerificationRepository } from '../repository/verification.repository';
 import { VerificationController } from '../verification.controller';
 import { VerificationService } from '../verification.service';
 
+const mockDataRequest = {
+  "company_id": "615bd16f09048c7954ee3d2f",
+  "documents": [],
+}
+
+const mockDataRequestError = {
+  "company_id": "1234567",
+  "documents": [],
+}
+
 const RepositoryMock = {
-  create: (dto) => {
-    return dto;
+  findOneAndUpdate: (dto) => {
+    if(dto._id === '615bd16f09048c7954ee3d2f') {
+      return dto;
+    }
+    else {
+      throw new Error("Bad Request Exception");
+    }
+    
   },
+
+  findById: (id) => {
+    if(id === '615bd16f09048c7954ee3d2f') {
+      return mockDataRequest;
+    }
+    else {
+      throw new Error("Bad Request Exception");
+    }
+
+  }
 }
 
 describe('VerificationController', () => {
@@ -30,5 +56,18 @@ describe('VerificationController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('shoud add or edit verification data', async () => {
+    expect(await controller.requestVerification(mockDataRequest)).toStrictEqual({"_id": "615bd16f09048c7954ee3d2f"});
+  })
+
+  it('should throw an error when add verification data', () => {
+    expect(controller.requestVerification(mockDataRequestError)).rejects.toThrow('Bad Request Exception');
+  });
+
+
+  it('should throw an error when get verification data', () => {
+    expect(controller.getVerificationData('1')).rejects.toThrow('Bad Request Exception');
   });
 });
--- src/verification/specs/verification.service.spec.ts
+++ src/verification/specs/verification.service.spec.ts
@@ -3,11 +3,24 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { VerificationRepository } from '../repository/verification.repository';
 import { VerificationService } from '../verification.service';
 
+const mockDataRequest = {
+  "company_id": "615bd16f09048c7954ee3d2f",
+  "documents": [],
+}
+
 const RepositoryMock = {
-  create: (dto) => {
+  findOneAndUpdate: (dto) => {
     return dto;
   },
+
+  findById: (id) => {
+    if(id) {
+      return mockDataRequest;
+    }
+  }
 }
+
+
 
 describe('VerificationService', () => {
   let service: VerificationService;
@@ -29,4 +42,16 @@ describe('VerificationService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('shoud add or edit verification data', async () => {
+    const reqVerification = await service.requestVerification(mockDataRequest);
+
+    expect(reqVerification._id).toBe(mockDataRequest.company_id);
+  })
+
+  it('should add verification data', async() => {
+    const getVerificationData = await service.getVerificationData('615bd16f09048c7954ee3d2f');
+
+    expect(getVerificationData.company_id).toBe('615bd16f09048c7954ee3d2f');
+  })
 });
